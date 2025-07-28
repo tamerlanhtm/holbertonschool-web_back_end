cfcb498e7ac45e7cfe73adb370627c93df0ffb3e
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-07-28 09:17:10 +0400

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    This module with typing Python
+    that contains `add(float, float) -> float` with type annotation.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+        Args:
+            a: The first number (float).
+            b: The second number (float).
+        Return:
+            The sum of `a` and `b` (float).
+    """
+    return a + b
