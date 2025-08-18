4170e36e3a484970e4a35eded4ec366f2334ca8a
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-08-18 11:45:59 +0000

    0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
-export default class Classroom {
-	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
+
